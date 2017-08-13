f2f7873db49ad847d7e76fe79249956aeb359224
evgeniy2194 <evgeniy2194@gmail.com>
2017-08-13 17:04:59 +0300

    dev

--- src/components/createform.js
+++ src/components/createform.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactCrop from 'react-image-crop';
 import 'react-image-crop/dist/ReactCrop.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class CreateForm extends React.Component {
 
@@ -10,7 +11,9 @@ class CreateForm extends React.Component {
         this.state = {
             data_uri: null,
             filename: null,
-            filetype: null
+            filetype: null,
+            crop: {aspect: 16 / 9},
+            pixelCrop: {}
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -59,89 +62,99 @@ class CreateForm extends React.Component {
     }
 
     onComplete(crop, pixelCrop) {
-        this.setState({
-            pixelCrop: pixelCrop
-        });
+        this.setState({crop, pixelCrop});
     }
 
     render() {
-
-        const crop = {
-            aspect: 16 / 9
-        };
-
         return (
-            <form className="form form-horizontal">
-                <div className="form-group form-group-sm">
-                    <div className="col-md-12">
-                        <label>Вопрос:</label>
-                        <textarea className="form-control"
-                                  name="question"
-                                  onChange={this.handleChange}
-                                  value={this.state.question}/>
-                    </div>
+            <div className="card">
+                <div className="card-header">
+                    Добавить вопрос
                 </div>
+                <div className="card-body">
+                    <form className="form form-horizontal">
+                        <div className="form-group row">
+                            <label className="col-sm-3 col-form-label">Вопрос:</label>
+                            <div className="col-sm-9">
+                            <textarea className="form-control form-control-sm"
+                                      name="question"
+                                      onChange={this.handleChange}
+                                      value={this.state.question}/>
+                            </div>
+                        </div>
 
-                <div className="form-group form-group-sm">
-                    <div className="col-md-12">
-                        <label>Категория:</label>
-                        <select className="form-control"
-                                name="category"
-                                onChange={this.handleChange}
-                                value={this.state.category}>
-                            <option value="1">Категория 1</option>
-                            <option value="2">Категория 2</option>
-                        </select>
-                    </div>
-                </div>
+                        <div className="form-group row">
+                            <label className="col-sm-3 col-form-label">Категория:</label>
+                            <div className="col-sm-9">
+                                <select className="form-control form-control-sm"
+                                        name="category"
+                                        onChange={this.handleChange}
+                                        value={this.state.category}>
+                                    <option value="1">Категория 1</option>
+                                    <option value="2">Категория 2</option>
+                                </select>
+                            </div>
+                        </div>
 
-                <div className="form-group form-group-sm">
-                    <div className="col-md-12">
-                        <label>Ответы:</label>
-                    </div>
-                    <div className="col-md-6 col-xs-6">
-                        <textarea className="form-control"
-                                  name="answer1"
-                                  onChange={this.handleChange}
-                                  value={this.state.answer1}/>
-                    </div>
-                    <div className="col-md-6 col-xs-6">
-                        <textarea className="form-control"
-                                  name="answer2"
-                                  onChange={this.handleChange}
-                                  value={this.state.answer2}/>
-                    </div>
-                </div>
-                <div className="form-group form-group-sm">
-                    <div className="col-md-6 col-xs-6">
-                        <textarea className="form-control"
-                                  name="answer3"
-                                  onChange={this.handleChange}
-                                  value={this.state.answer3}/>
-                    </div>
-                    <div className="col-md-6 col-xs-6">
-                        <textarea className="form-control"
-                                  name="answer4"
-                                  onChange={this.handleChange}
-                                  value={this.state.answer4}/>
-                    </div>
-                </div>
+                        <div className="form-group row">
+                            <label className="col-sm-3 col-form-label">Ответы:</label>
+                            <div className="col-sm-9">
+                                <input className="form-control form-control-sm"
+                                       name="answer1"
+                                       onChange={this.handleChange}
+                                       value={this.state.answer1}/>
+                            </div>
+                        </div>
 
-                <div className="form-group form-group-sm">
-                    <div className="col-xs-6">
-                        <label>Изображение:</label>
-                        <input type="file" onChange={this.onInputFileChange.bind(this)}
-                               value={this.state.originalImage}/>
-                        <ReactCrop src={this.state.data_uri} crop={crop} onComplete={this.onComplete.bind(this)}/>
-                    </div>
-                    <div className="col-xs-6">
-                        <button className="btn btn-sm btn-default pull-right"
+                        <div className="form-group row">
+                            <div className="col-sm-9 ml-auto">
+                                <input className="form-control form-control-sm"
+                                       name="answer2"
+                                       onChange={this.handleChange}
+                                       value={this.state.answer2}/>
+                            </div>
+                        </div>
+
+                        <div className="form-group row">
+                            <div className="col-sm-9 ml-auto">
+                                <input className="form-control form-control-sm"
+                                       name="answer3"
+                                       onChange={this.handleChange}
+                                       value={this.state.answer3}/>
+                            </div>
+                        </div>
+
+                        <div className="form-group row">
+                            <div className="col-sm-9 ml-auto">
+                                <input className="form-control form-control-sm"
+                                       name="answer4"
+                                       onChange={this.handleChange}
+                                       value={this.state.answer4}/>
+                            </div>
+                        </div>
+
+                        <div className="form-group row">
+                            <label className="col-sm-3 col-form-label">Изображение:</label>
+                            <div className="col-sm-9">
+                                <input className="form-control form-control-sm" type="file" onChange={this.onInputFileChange.bind(this)}
+                                       value={this.state.originalImage}/>
+                            </div>
+                        </div>
+
+                        <div className="form-group row">
+                            <ReactCrop src={this.state.data_uri} crop={this.state.crop}
+                                       onComplete={this.onComplete.bind(this)}/>
+                        </div>
+
+                        <div className="form-group row">
+                            <button className="btn btn-sm btn-default ml-auto"
                                 onClick={this.submit.bind(this)}>Добавить
-                        </button>
-                    </div>
+                            </button>
+                        </div>
+                    </form>
                 </div>
-            </form>
-        );
+            </div>
+        )
     }
 }
 
